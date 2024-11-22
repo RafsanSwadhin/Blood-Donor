cb3a8fc752849d10d5912e665d44c1b7bdb3fd9c
Rafsan-Swadhin <rafsanjaniahmed84@gmail.com>
2024-11-22 22:08:40 +0600

    2

--- app/models.py
+++ app/models.py
@@ -18,6 +18,74 @@ class Post(models.Model):
         ('A+','A+'),
         ('A-','A-'),
     )
+    CITY = (
+    ('Dhaka', 'Dhaka'),
+    ('Faridpur', 'Faridpur'),
+    ('Gazipur', 'Gazipur'),
+    ('Gopalganj', 'Gopalganj'),
+    ('Jamalpur', 'Jamalpur'),
+    ('Kishoreganj', 'Kishoreganj'),
+    ('Madaripur', 'Madaripur'),
+    ('Manikganj', 'Manikganj'),
+    ('Munshiganj', 'Munshiganj'),
+    ('Mymensingh', 'Mymensingh'),
+    ('Narayanganj', 'Narayanganj'),
+    ('Narsingdi', 'Narsingdi'),
+    ('Netrokona', 'Netrokona'),
+    ('Rajbari', 'Rajbari'),
+    ('Shariatpur', 'Shariatpur'),
+    ('Sherpur', 'Sherpur'),
+    ('Tangail', 'Tangail'),
+    ('Bogra', 'Bogra'),
+    ('Joypurhat', 'Joypurhat'),
+    ('Naogaon', 'Naogaon'),
+    ('Natore', 'Natore'),
+    ('Nawabganj', 'Nawabganj'),
+    ('Pabna', 'Pabna'),
+    ('Rajshahi', 'Rajshahi'),
+    ('Sirajgonj', 'Sirajgonj'),
+    ('Dinajpur', 'Dinajpur'),
+    ('Gaibandha', 'Gaibandha'),
+    ('Kurigram', 'Kurigram'),
+    ('Lalmonirhat', 'Lalmonirhat'),
+    ('Nilphamari', 'Nilphamari'),
+    ('Panchagarh', 'Panchagarh'),
+    ('Rangpur', 'Rangpur'),
+    ('Thakurgaon', 'Thakurgaon'),
+    ('Barguna', 'Barguna'),
+    ('Barisal', 'Barisal'),
+    ('Bhola', 'Bhola'),
+    ('Jhalokati', 'Jhalokati'),
+    ('Patuakhali', 'Patuakhali'),
+    ('Pirojpur', 'Pirojpur'),
+    ('Bandarban', 'Bandarban'),
+    ('Brahmanbaria', 'Brahmanbaria'),
+    ('Chandpur', 'Chandpur'),
+    ('Chittagong', 'Chittagong'),
+    ('Comilla', 'Comilla'),
+    ('Cox\'s Bazar', 'Cox\'s Bazar'),
+    ('Feni', 'Feni'),
+    ('Khagrachari', 'Khagrachari'),
+    ('Lakshmipur', 'Lakshmipur'),
+    ('Noakhali', 'Noakhali'),
+    ('Rangamati', 'Rangamati'),
+    ('Habiganj', 'Habiganj'),
+    ('Maulvibazar', 'Maulvibazar'),
+    ('Sunamganj', 'Sunamganj'),
+    ('Sylhet', 'Sylhet'),
+    ('Bagerhat', 'Bagerhat'),
+    ('Chuadanga', 'Chuadanga'),
+    ('Jessore', 'Jessore'),
+    ('Jhenaidah', 'Jhenaidah'),
+    ('Khulna', 'Khulna'),
+    ('Kushtia', 'Kushtia'),
+    ('Magura', 'Magura'),
+    ('Meherpur', 'Meherpur'),
+    ('Narail', 'Narail'),
+    ('Satkhira', 'Satkhira'),
+)
+
+
     # user = models.OneToOneField(User,on_delete=models.CASCADE,blank=True,null=True)
     user = models.ForeignKey(User,on_delete=models.CASCADE,blank=True,null=True)
     id = models.AutoField(primary_key=True)
@@ -27,6 +95,7 @@ class Post(models.Model):
     details = models.TextField()
     # available = models.BooleanField()
     category = models.CharField(max_length=100, choices=CATEGORY)
+    city = models.CharField(max_length=100, choices=CITY)
     created_at = models.DateTimeField(default=now)
     # image = models.ImageField(default='default.jpg', upload_to='tuition/images')
     group = MultiSelectField(max_length=100,max_choices = 5, choices = GROUP)#default='Bangla')
